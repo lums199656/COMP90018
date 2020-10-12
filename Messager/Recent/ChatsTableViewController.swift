e453d1e0332fe504264a994260c705fdcf8595b8
Minshen Lu <lums19960506@gmail.com>
2020-10-12 13:46:27 +0800

    合并后的第一版

--- Messager/Recent/ChatsTableViewController.swift
+++ Messager/Recent/ChatsTableViewController.swift
@@ -12,7 +12,7 @@ class ChatsTableViewController: UITableViewController {
     
     var allRecents: [RecentChat] = []
     var filteredRecents: [RecentChat] = []
-
+    let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +24,8 @@ class ChatsTableViewController: UITableViewController {
         // 增加下拉更新的功能
         self.refreshControl = UIRefreshControl()
         self.tableView.refreshControl = self.refreshControl
+        
+        self.setupSearchController()
     }
 
     // MARK: - Table view data source
@@ -31,16 +33,16 @@ class ChatsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return allRecents.count
+        return searchController.isActive ? filteredRecents.count : allRecents.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RecentTableViewCell
-
+        
+        let recent = searchController.isActive ? filteredRecents[indexPath.row] : allRecents[indexPath.row]
         // Configure the cell...
-        cell.configure(recent: allRecents[indexPath.row])
-
+        cell.configure(recent: recent)
         return cell
     }
     
@@ -62,50 +64,28 @@ class ChatsTableViewController: UITableViewController {
         }
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    private func setupSearchController() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Chat"
+        searchController.searchResultsUpdater = self
+        definesPresentationContext = true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    // 搜索筛选的策略
+    private func filteredContentForSearchText(searchText: String) {
+        filteredRecents = allRecents.filter({ (recent) -> Bool in
+            return recent.receiverName.lowercased().contains(searchText.lowercased())
+        })
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+// 如何搜索
+extension ChatsTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+}
+
