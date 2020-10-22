c2782e1ee469add5fcbb17934f379b7e2a814d4b
pinwang5776 <pinwang5776@outlook.com>
2020-10-22 03:46:01 -0700

    before merge

--- Messager/Model/Activity/ActivityDetailController.swift
+++ Messager/Model/Activity/ActivityDetailController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ActivityDetailController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadData()
         // Do any additional setup after loading the view.
     }
     
@@ -39,14 +40,9 @@ class ActivityDetailController: UIViewController {
         
         
     }
-    /*
-     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func loadData() {
+        
     }
-    */
-
 }
