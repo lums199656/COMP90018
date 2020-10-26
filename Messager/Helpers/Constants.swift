cf2d7a75ab642426464e64954de6b4e4483d30a3
Minshen Lu <lums19960506@gmail.com>
2020-10-26 21:02:17 +0800

    No Bug v1

--- Messager/Helpers/Constants.swift
+++ Messager/Helpers/Constants.swift
@@ -37,3 +37,26 @@ public let kMEMBERIDS = "memberIds"
 
 
 
+struct K {
+    
+//    static let
+    
+    struct Store {
+        static let activityImage = "activity-images"
+    }
+    
+    struct FStore {
+        static let user = "Users"
+        static let act = "Activities"
+        static let app = "Applications"
+    }
+    
+    struct Activity {
+        static let detail = "actDetail"
+        static let title = "actTitle"
+        static let image = "imageId"
+        static let uid = "uid"
+        static let user = "userId"
+    }
+    
+}
